94eaf8a1b2164ba9659db6d0bf2e637236baf0d8
Alejandro Mallea <janoma@gmail.com>
2023-11-10 22:18:49 -0800

    implement the table with times

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import Autocomplete from "@mui/joy/Autocomplete";
 import CssBaseline from "@mui/joy/CssBaseline";
 import FormControl from "@mui/joy/FormControl";
 import GlobalStyles from "@mui/joy/GlobalStyles";
+import Link from "@mui/joy/Link";
 import Radio from "@mui/joy/Radio";
 import RadioGroup from "@mui/joy/RadioGroup";
+import Table from "@mui/joy/Table";
 import Typography from "@mui/joy/Typography";
 
 const moment = require("moment-timezone");
@@ -14,6 +16,8 @@ function App() {
   const [division, setDivision] = useState("open");
   const [region, setRegion] = useState("EMEAE Central");
   const [timezone, setTimezone] = useState(moment.tz.guess());
+  const [zrl, setZrl] = useState(null);
+  const [races, setRaces] = useState(null);
 
   const handleDivisionChange = (event) => {
     setDivision(event.target.value);
@@ -68,10 +72,35 @@ function App() {
   const timezones = moment.tz.names();
 
   useEffect(() => {
+    const fetchZrl = async () => {
+      fetch("./zrl.json")
+        .then((response) => response.json())
+        .then((data) => setZrl(data));
+    };
+    fetchZrl();
+  }, []);
+
+  useEffect(() => {
+    const fetchRaces = async () => {
+      fetch("./races.json")
+        .then((response) => response.json())
+        .then((data) => setRaces(data));
+    };
+    fetchRaces();
+  }, []);
+
+  useEffect(() => {
     if (division === "women" && !regions.includes(region)) {
       setRegion("EMEAE Central");
     }
   }, [division, region, regions]);
+
+  const canShowTimes =
+    moment.tz.zone(timezone) &&
+    races &&
+    zrl &&
+    zrl.hasOwnProperty(division) &&
+    zrl[division].hasOwnProperty(region);
 
   return (
     <CssVarsProvider>
@@ -82,7 +111,6 @@ function App() {
             maxWidth: "80ch",
             margin: "0 auto",
             padding: "1rem",
-            color: "var(--joy-text-primary)",
           },
           "#root": {
             display: "grid",
@@ -97,12 +125,13 @@ function App() {
           h3: {
             paddingBottom: "0.5rem",
           },
+          section: {
+            paddingTop: "2rem",
+          },
         }}
       />
       <header>
-        <Typography color="neutral" level="h1">
-          Zwift Racing League Local Time Checker
-        </Typography>
+        <Typography level="h1">Zwift Racing League Local Times</Typography>
       </header>
       <main>
         <FormControl className="question">
@@ -144,6 +173,50 @@ function App() {
             value={timezone}
           />
         </FormControl>
+        {canShowTimes && (
+          <section>
+            <Typography level="h2">Your race times</Typography>
+            <Table aria-label="basic table">
+              <thead>
+                <tr>
+                  <th style={{ width: "5%" }}>#</th>
+                  <th>Date</th>
+                  <th>Time</th>
+                  <th>Type</th>
+                  <th style={{ width: "40%" }}>Course</th>
+                </tr>
+              </thead>
+              <tbody>
+                {races.map((row) => (
+                  <tr key={row.number}>
+                    <td>{row.number}</td>
+                    <td>{moment(row.date).tz(timezone).format("ll")}</td>
+                    <td>
+                      {moment
+                        .tz(
+                          `${row.date} ${zrl[division][region].time}`,
+                          zrl[division][region].timezone
+                        )
+                        .tz(timezone)
+                        .format("LT")}
+                    </td>
+                    <td>{row.type}</td>
+                    <td>
+                      <Link
+                        href={row.courseUrl}
+                        color="neutral"
+                        target="_blank"
+                        rel="noopener"
+                      >
+                        {row.course}
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </Table>
+          </section>
+        )}
       </main>
     </CssVarsProvider>
   );
