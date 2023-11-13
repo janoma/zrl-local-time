4c34b1bcb964b3dede9cb38114b741f333a24ed7
Alejandro Mallea <janoma@gmail.com>
2023-11-13 07:56:36 -0800

    remove unused import

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useEffect, useState } from "react";
 
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
